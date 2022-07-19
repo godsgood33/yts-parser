dd6de2501efd8252f85b38d46bacc64fe4680cb6
Ryan Prather <godsgood33@gmail.com>
2022-07-19 17:23:23 -0400

    DotEnv update

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require_once dirname(__DIR__).'/vendor/autoload.php';
 
 use YTS\DotEnv;
 
+DotEnv::$DEFINE = false;
+DotEnv::$LOAD_ENV = false;
 DotEnv::load(dirname(__DIR__).'/.env');
 
 require_once dirname(__DIR__).'/config/routes.php';
