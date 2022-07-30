5ad809bbc7989d9566aa31f03d684d8951b743fd
Ryan Prather <godsgood33@gmail.com>
2022-07-30 19:35:46 -0400

    Several updates

--- yts-parser.php
+++ yts-parser.php
@@ -2,12 +2,21 @@
 
 require_once __DIR__."/vendor/autoload.php";
 
+use jc21\PlexApi;
+use jc21\Section;
 use YTS\Movie;
 use YTS\Plex;
 use YTS\TransServer;
 use YTS\YTS;
 use YTS\DotEnv;
 use YTS\CMD;
+
+if (!file_exists(__DIR__.'/.env')) {
+    touch(__DIR__.'/.env');
+}
+
+DotEnv::$LOAD_ENV = false;
+DotEnv::$LOAD_SERVER = false;
 
 DotEnv::load(__DIR__.'/.env');
 $cmd = CMD::getCommandParameters();
@@ -22,6 +31,48 @@ if ($cmd->install) {
     die("Created database table in movies.db");
 }
 
+if ($cmd->plexToken) {
+    if (!Plex::validateEnvironment()) {
+        die('Need to populate .env file with correct values');
+    }
+
+    $api = new PlexApi($_ENV['PLEX_SERVER']);
+    $api->setAuth($_ENV['PLEX_USER'], $_ENV['PLEX_PASSWORD']);
+
+    $token = $api->getToken();
+    $env = file_get_contents(".env");
+    $env .= PHP_EOL."PLEX_TOKEN={$token}".PHP_EOL;
+    file_put_contents(".env", $env);
+
+    print ".env file updated with PLEX_TOKEN so it doesn't have to authenticate every time".PHP_EOL;
+    die;
+}
+
+if ($cmd->libraryList) {
+    if (!Plex::validateEnvironment()) {
+        die('Failed to validate all environment variables are present');
+    }
+    $api = new PlexApi($_ENV['PLEX_SERVER']);
+    $api->setToken($_ENV['PLEX_TOKEN']);
+
+    $res = $api->getLibrarySections();
+
+    print "ID\tTitle\tPath".PHP_EOL;
+
+    if ($res['size'] > 1) {
+        foreach ($res['Directory'] as $s) {
+            $section = Section::fromLibrary($s);
+            print $section->key."\t".$section->title."\t".$section->location->path.PHP_EOL;
+        }
+    }
+
+    $id = readline("Which library do you want to query when searching for movies? ");
+
+    $env = file_get_contents(".env");
+    $env .= "PLEX_MOVIE_LIBRARY={$id}".PHP_EOL;
+    file_put_contents(".env", $env);
+}
+
 if ($cmd->update) {
     $page = $cmd->startPage;
     $keepGoing = true;
@@ -31,10 +82,12 @@ if ($cmd->update) {
     $fhdMovies = 0;
     $uhdMovies = 0;
     $yts = new YTS();
-    $plex = new Plex();
-    if ($cmd->plexDB) {
-        $plex->setDB($cmd->plexDB);
+    $api = null;
+    if (Plex::validateEnvironment()) {
+        $api = new PlexApi($_ENV['PLEX_SERVER']);
+        $api->setToken($_ENV['PLEX_TOKEN']);
     }
+    $plex = new Plex($api);
 
     do {
         print "Loading page $page".PHP_EOL;
@@ -45,7 +98,7 @@ if ($cmd->update) {
         if (!count($movies)) {
             $keepGoing = false;
         }
-        
+
         foreach ($movies as $movie) {
             $imgLink = $movie->parent->parent->find('.img-responsive');
             $lang = $movie->find('span');
@@ -81,14 +134,14 @@ if ($cmd->update) {
                 $nm->retrieved = true;
                 $yts->addMovie($nm);
             }
-            
+
             if ($cmd->torrentLinks) {
                 $yts->getTorrentLinks($nm);
             }
 
             if ($plex->isConnected()) {
                 $onPlex = $plex->check($nm);
-                
+
                 if ($onPlex) {
                     $nm->setDownload($onPlex);
                 }
@@ -115,8 +168,14 @@ if ($cmd->update) {
         }
     } while ($keepGoing);
 
-    print "New movies: {$newMovie}".PHP_EOL;
-    print "Existing movies: {$existingMovie}".PHP_EOL;
+    print <<<EOF
+    HD movies: {$hdMovies}
+    FHD movies: {$fhdMovies}
+    UHD movies: {$uhdMovies}
+    New movies: {$newMovie}
+    Existing movies: {$existingMovie}
+    
+    EOF;
 }
 
 if ($cmd->download) {
@@ -124,7 +183,7 @@ if ($cmd->download) {
     $movies = $yts->getDownloadableMovies();
     $ts = new TransServer();
 
-    if (!is_countable($movies) && count($movies)) {
+    if (!is_countable($movies) && $movies->count()) {
         die("No movies found");
     }
 
@@ -176,11 +235,5 @@ if ($cmd->highestVersion) {
         $yts->getTorrentLinks($movie);
         $yts->updateMovie($movie);
         $idx++;
-    }
-}
-
-if ($cmd->updatePlex) {
-    if (!$cmd->plexDB) {
-        die(YTS::usage());
     }
 }
