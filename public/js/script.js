b3bd676de5cb6cfd8504c7476f9027c3c50258fd
Ryan Prather <godsgood33@gmail.com>
2022-07-30 20:18:29 -0400

    Change search method to use router

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
 $(function () {
     $('#download').click(updateDownload);
     $('.download').click(download);
+    $('.delete').click(deleteMovie);
     $('#search').keypress(search);
+    ts = setTimeout(torrentStatus, 1000);
 });
 
 function updateDownload() {
@@ -28,10 +30,9 @@ function search(event) {
     if (event.which != 13) return;
     if ($('#search').val().length < 2) return;
 
-    $.ajax('/query.php', {
+    $.ajax('/search', {
         data: {
-            action: 'search',
-            term: $('#search').val()
+            term: encodeURIComponent($('#search').val())
         },
         success: function (res) {
             $('#container').children().remove();
@@ -48,7 +49,7 @@ function search(event) {
 
 function download() {
     var btn = $(this);
-    $.ajax('/query.php', {
+    $.ajax('/download', {
         data: {
             action: 'download',
             title: $(this).data('title'),
@@ -67,4 +68,52 @@ function download() {
         method: 'post',
         dataType: 'json'
     });
+}
+
+function deleteMovie() {
+    var btn = $(this);
+    $.ajax('/deleteMovie', {
+        data: {
+            title: $(this).data('title'),
+            year: $(this).data('year')
+        },
+        success: function (res) {
+            if (res.success) {
+                alert(res.title + ' (' + res.year + ') deleted');
+                $(btn).parent().remove();
+                return;
+            }
+            console.log(res);
+        },
+        error: function (xhr, error, msg) {
+            console.error(msg);
+        },
+        method: 'post',
+        dataType: 'json'
+    });
+}
+
+function torrentStatus() {
+    $.ajax('/status', {
+        data: {},
+        success: function (res) {
+            $('#downloadSize').html(res.downloadSize + "GB");
+            $('#freeSpace').html(res.freeSpace + "GB");
+            ts = setTimeout(torrentStatus, 1000);
+        },
+        error: function (xhr, error, msg) {
+            console.error(msg);
+        },
+        method: 'post',
+        dataType: 'json'
+    });
+}
+
+function toggleStatus() {
+    if (!ts) {
+        ts = setTimeout(torrentStatus, 1000);
+    } else {
+        clearTimeout(ts);
+        ts = null;
+    }
 }
